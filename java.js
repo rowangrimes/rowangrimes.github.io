6d4950c2cdb98550abbc6c83f373e9293dd5da40
rowangrimes <102987765+rowangrimes@users.noreply.github.com>
2022-04-04 22:38:02 +0100

    visual update 1

--- java.js
+++ java.js
@@ -5,6 +5,26 @@ $(function(){
     $('#footer').load('footer.html');
 });
 
+var i = 0;
+var images = [];
+var slideTime = 10000; // 3 seconds
+
+images[0] = 'websitebackground1.jpg';
+images[1] = 'websitebackground2.jpg';
+images[2] = 'websitebackground3.jpg';
+
+function changePicture() {
+    document.body.style.backgroundImage = "url(" + images[i] + ")";
+
+    if (i < images.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+    setTimeout(changePicture, slideTime);
+}
+
+window.onload = changePicture;
 
 
 
